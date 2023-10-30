fb6b02d3db486349d5e47876c17a06d8960a0290
LL-00010414 <allhater37@gmail.com>
2023-10-30 19:28:10 +0500

    adding addAnime screen

--- lib/screens/anime.dart
+++ lib/screens/anime.dart
@@ -1,21 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:per_rat/data/movie_info.dart';
 
 import 'package:per_rat/models/anime.dart';
 import 'package:per_rat/screens/anime_details.dart';
+import 'package:per_rat/screens/new_anime.dart';
 import 'package:per_rat/widgets/anime_item.dart';
 //import 'package:per_rat/widgets/anime_item.dart';
 //import 'package:per_rat/widgets/anime_item_trait.dart';
 //import 'package:per_rat/data/movie_info.dart';
 //import 'package:transparent_image/transparent_image.dart';
 
-class AnimeScreen extends StatelessWidget {
+class AnimeScreen extends StatefulWidget {
   const AnimeScreen({
     super.key,
     required this.chosenAnime,
   });
 
   final List<Anime> chosenAnime;
-  //final List<Anime> availableList;
+
+  @override
+  State<AnimeScreen> createState() => _AnimeScreenState();
+}
+
+class _AnimeScreenState extends State<AnimeScreen> {
+  final List<Anime> _registeredAnime = dummyAnime;
+
+  void _addAnime(Anime anime) {
+    setState(() {
+      _registeredAnime.add(anime);
+    });
+  }
+
+  void _removeAnime(Anime anime) {
+    // final List<String> videoURL =
+    // dummyAnime.map((anime) => anime.trailerUrl).toList();
+    //final animeName = _registeredAnime.map((anime) => anime.title).toList();
+    final animeIndex = _registeredAnime.indexOf(anime);
+    setState(() {
+      _registeredAnime.remove(anime);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: Text("${anime.title} has been deleted"),
+        action: SnackBarAction(
+            label: "Undo",
+            onPressed: () {
+              setState(() {
+                _registeredAnime.insert(animeIndex, anime);
+              });
+            }),
+      ),
+    );
+  }
+
+  void _openAddAnimeDialog() {
+    showModalBottomSheet(
+        context: context, builder: (ctx) => NewAnime(onAddAnime: _addAnime));
+  }
 
   void selectAnime(BuildContext context, Anime anime) {
     Navigator.of(context).push(
@@ -36,9 +80,9 @@ class AnimeScreen extends StatelessWidget {
         crossAxisSpacing: 5,
         mainAxisSpacing: 5,
       ),
-      itemCount: chosenAnime.length,
+      itemCount: widget.chosenAnime.length,
       itemBuilder: (ctx, index) => AnimeItem(
-        anime: chosenAnime[index],
+        anime: widget.chosenAnime[index],
         onSelectAnime: (anime) {
           selectAnime(context, anime);
         },
